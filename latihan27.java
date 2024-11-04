b9968050bb33e18ad5b593f82002d55f750abac6
Nur Riskon Abdan Syakuro <23176031@mhs.stmik-tegal.ac.id>
2024-11-04 19:08:33 +0700

    Add files via upload

---
+++ latihan27.java
@@ -0,0 +1,27 @@
+//Nama      : Nur Riskon Abdan Syakuro
+//  NIM       : 23176031
+//  Prodi     : Sistem Informasi
+//  Deskripsi :   program ini untuk mengubah huruf besar dan huruf kecil
+
+
+import java.util.Scanner;
+
+public class latihan27 {
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+          System.out.println("run:");
+          System.out.print("Masukkan Kalimat : ");
+          String kalimat = scanner.nextLine();
+
+          String hurufBesar = kalimat.toUpperCase();
+          String hurufKecil = kalimat.toLowerCase();
+
+          System.out.println("====Hasil Formatting====");
+          System.out.println("Huruf Besar : " + hurufBesar);
+          System.out.println("Huruf Kecil : " + hurufKecil);
+        }
+
+        System.out.println("BUILD SUCCESSFUL (total time: 2 menit");
+    }
+}
